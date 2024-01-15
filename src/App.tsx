a0ee339de7181f695b5a69e21931f9a366d95a01
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-01-15 13:47:37 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Paragraph from "./Paragraph";
+
+const styles = {
+  borderColor: "black",
+  borderStyle: "solid",
+  borderWidth: "1px",
+}
+
+function App() {
+  const name = "Fabio";
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p style={styles} className="red">
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <Paragraph>Contenu enfant de la balise Paragraph</Paragraph>
+      </div>
+      <p className="read-the-docs">
+        Learn React with {name} - {3 + 4} - Tableau: {["useState", "useEffect"]} 
+        - Boolean: {true ? 'true' : 'false'} - Null: {null} - Undefined: {undefined}
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Paragraph.tsx
@@ -0,0 +1,9 @@
+import myClassnames from "./App.module.css"; // import css global
+
+const Paragraph = ({ children }: { children?: React.ReactNode }) => {
+    return (
+        <p className={myClassnames.red}>{children}</p>
+    )
+}
+
+export default Paragraph
