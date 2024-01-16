a233bec0683b605cd8e89c197b30e21e7a562b21
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-01-16 10:54:10 +0100

    feat: add spinner to HeroesList

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import classnames from './spinner.module.css'
+
+const Spinner = () => {
+  return (
+    <div className={classnames.loader}></div>
+  )
+}
+
+export default Spinner
--- src/pages/HeroesList.tsx
+++ src/pages/HeroesList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Hero } from "../types/hero";
 import { getHeroesByLetter } from "../api/heroes";
 import HeroCard from "../components/HeroCard";
+import Spinner from "../components/Spinner/Spinner";
 
 const arrayOfLetters: string[] = [];
 for (let index = 65; index < 91; index++) {
@@ -15,29 +16,32 @@ for (let index = 65; index < 91; index++) {
 // Ne modifie pas le scope
 
 const getActiveClassName = (condition: boolean) => {
-  return condition ? "text-red-600 cursor-pointer" : "cursor-pointer"
-}
+  return condition ? "text-red-600 cursor-pointer" : "cursor-pointer";
+};
 
 const HeroesList = () => {
   // Declaration des useState
   const [selectedLetter, setSelectedLetter] = useState<string>("A");
   const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   // Declaration des useEffect
   useEffect(() => {
-    const controller = new AbortController()
+    const controller = new AbortController();
     getHeroesByLetter("A", { signal: controller.signal }).then((data) => {
-      setHeroes(data)
-    })
+      setLoading(false);
+      setHeroes(data);
+    });
     return () => {
-      controller.abort()
-    }
-  }, [])
-
+      controller.abort();
+    };
+  }, []);
 
   const onClickHandler = (letter: string) => {
     setSelectedLetter(letter);
+    setLoading(true);
     getHeroesByLetter(letter).then((data) => {
+      setLoading(false);
       setHeroes(data);
     });
   };
@@ -47,17 +51,24 @@ const HeroesList = () => {
       <h1>Heroes List</h1>
       <ul className="flex justify-center gap-3 text-xl font-semibold">
         {arrayOfLetters.map((letter) => (
-          <li key={letter} onClick={() => onClickHandler(letter)} className={getActiveClassName(letter === selectedLetter)}>
+          <li
+            key={letter}
+            onClick={() => onClickHandler(letter)}
+            className={getActiveClassName(letter === selectedLetter)}
+          >
             {letter}
           </li>
         ))}
       </ul>
       <p>Vous avez cliqué sur la lettre: {selectedLetter}</p>
-      <div className="flex justify-center flex-wrap gap-6">
-        {heroes.map((hero) => (
-          <HeroCard key={hero.id} hero={hero} />
-        ))}
-      </div>
+      {loading && <Spinner />}
+      {!loading && (
+        <div className="flex justify-center flex-wrap gap-6">
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} hero={hero} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
