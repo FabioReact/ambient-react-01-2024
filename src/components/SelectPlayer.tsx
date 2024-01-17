158f556940e481efeb0fb4f8ab825c6476c5df94
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-01-17 15:31:21 +0100

    feat: add SelectPlayer component

---
+++ src/components/SelectPlayer.tsx
@@ -0,0 +1,64 @@
+import { queryOptions, useQuery } from '@tanstack/react-query'
+import { useRef } from 'react'
+import { searchHeroes } from '../api/heroes'
+import Loading from './Loading'
+import { Hero } from '../types/hero'
+
+type HeroLabelProps = {
+    id: number
+    name: string
+    onClick: () => void
+}
+
+const HeroLabel = ({ id, name, onClick }: HeroLabelProps) => {
+    return (
+        <p onClick={onClick} className='border rounded p-2 my-1 border-gray-700 cursor-pointer hover:bg-gray-100'>
+        <span className='font-semi-bold text-gray-500 pr-2'>{id}</span>
+        {name}
+      </p>
+    )
+}
+
+type SelectPlayerProps = {
+  label?: string
+  setPlayer: (hero: Hero) => void
+}
+
+const SelectPlayer = ({ label = '', setPlayer }: SelectPlayerProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const { data, isLoading, isFetching, isError, refetch } = useQuery(
+    queryOptions({
+      queryKey: ['heroesByName', inputRef.current?.value, label],
+      queryFn: () => searchHeroes(inputRef.current?.value || ''),
+      enabled: !!inputRef.current?.value,
+    }),
+  )
+
+  const onSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+    if (inputRef.current?.value) refetch()
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor={`select-player-${label}`}>Select Player {label}</label>
+        <input
+          type='text'
+          id={`select-player-${label}`}
+          name={`select-player-${label}`}
+          ref={inputRef}
+        />
+        <button type='submit'>Search</button>
+      </form>
+      {isError && <p className='text-red-500'>An error occured</p>}
+      <Loading isLoading={isLoading || isFetching}>
+        <div className='inline-block'>
+          {data?.map((hero) => <HeroLabel id={hero.id} name={hero.name} onClick={() => setPlayer(hero)} />)}
+        </div>
+      </Loading>
+    </div>
+  )
+}
+
+export default SelectPlayer
--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,60 +1,24 @@
-import { queryOptions, useQuery } from '@tanstack/react-query'
-import { useRef } from 'react'
-import { searchHeroes } from '../api/heroes'
-import Loading from '../components/Loading'
-
-type SelectPlayerProps = {
-  label?: string
-}
-
-const SelectPlayer = ({ label = '' }: SelectPlayerProps) => {
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const { data, isLoading, isFetching, isError, refetch } = useQuery(
-    queryOptions({
-      queryKey: ['heroesByName', inputRef.current?.value],
-      queryFn: () => searchHeroes(inputRef.current?.value || ''),
-      enabled: !!inputRef.current?.value,
-    }),
-  )
-
-  const onSubmitHandler = (event: React.FormEvent) => {
-    event.preventDefault()
-    if (inputRef.current?.value) refetch()
-  }
-  return (
-    <div>
-      <form onSubmit={onSubmitHandler}>
-        <label htmlFor={`select-player-${label}`}>Select Player {label}</label>
-        <input
-          type='text'
-          id={`select-player-${label}`}
-          name={`select-player-${label}`}
-          ref={inputRef}
-        />
-        <button type='submit'>Search</button>
-      </form>
-      {isError && <p className='text-red-500'>An error occured</p>}
-      <Loading isLoading={isLoading || isFetching}>
-        <div className='inline-block'>
-          {data?.map((hero) => (
-            <p className='border rounded p-2 my-1 border-gray-700 cursor-pointer hover:bg-gray-100'>
-              <span className='font-semi-bold text-gray-500 pr-2'>{hero.id}</span>
-              {hero.name}
-            </p>
-          ))}
-        </div>
-      </Loading>
-    </div>
-  )
-}
+import { useState } from "react"
+import SelectPlayer from "../components/SelectPlayer"
+import { Hero } from "../types/hero"
+import HeroCard from "../components/HeroCard"
 
 const Battle = () => {
+  const [playerOne, setPlayerOne] = useState<Hero|null>(null)
+  const [playerTwo, setPlayerTwo] = useState<Hero|null>(null)
   return (
-    <section>
+    <section className="flex flex-col items-center">
       <h1>Battle</h1>
-      <SelectPlayer label='one' />
-      <SelectPlayer label='two' />
+      <div className="flex justify-center gap-24">
+        <SelectPlayer label='one' setPlayer={setPlayerOne} />
+        <SelectPlayer label='two' setPlayer={setPlayerTwo} />
+      </div>
+      {playerOne && playerTwo &&
+        <button>Start Battle</button>}
+      <div className="flex justify-center gap-24">
+        {playerOne && <HeroCard hero={playerOne} />}
+        {playerTwo && <HeroCard hero={playerTwo} />}
+      </div>
     </section>
   )
 }
